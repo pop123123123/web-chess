581141c4151d523cf8face395e6f791673551c17
Antoine Brûlé <antoine.brule@netc.eu>
2021-05-29 21:22:11 +0200

    move chess pieces attribution to about page

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <p><a href="https://commons.wikimedia.org/wiki/File:Chess_Pieces_Sprite.svg">Chess pieces by Cburnett</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons</p>
   </div>
 </template>
--- front/src/views/Game.vue
+++ front/src/views/Game.vue
@@ -2,7 +2,6 @@
   <div class="game">
     <h1>Game</h1>
     <Board :state="state.board" />
-    <p><a href="https://commons.wikimedia.org/wiki/File:Chess_Pieces_Sprite.svg">Chess pieces by Cburnett</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons</p>
   </div>
 </template>
 
