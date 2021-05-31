61cc846b654ab1ae5546428738f5e759df983e92
Antoine Brûlé <antoine.brule@netc.eu>
2021-05-31 23:07:33 +0200

    add explicit eslintrc rules for conflicting semicolon rules

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
