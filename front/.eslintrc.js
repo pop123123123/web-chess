c313efb91f3072676319bfa468dbbb73f4439925
Antoine Brûlé <antoine.brule@netc.eu>
2021-06-01 20:42:33 +0200

    fix no-shadow lint rule

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
   rules: {
     semi: 'off',
     '@typescript-eslint/semi': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
