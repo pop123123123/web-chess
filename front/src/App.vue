d63f24b5f1cfaa548f5ebad684abd218201dc154
Louis MOUHAT <louis.mouhat@gmail.com>
2021-06-05 16:38:03 +0200

    enlarge navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -16,7 +16,6 @@ export default defineComponent({
 
 <style lang="scss">
 @use './scss/theme';
-$hover-color: #556b2f44;
 
 body {
   display: flex;
@@ -32,31 +31,6 @@ body {
   margin: 0;
   color: theme.$color-text;
   font-family: sans-serif;
-}
-
-#nav {
-  background: theme.$background-header;
-  color: theme.$color-text-dark;
-  padding: 1em;
-  text-align: center;
-
-  a {
-    text-decoration: none;
-    padding: .5em 1.5em;
-    margin: .5em .5em;
-    text-transform: lowercase;
-    font-variant: small-caps;
-    transition: 0.2s;
-
-    &:hover, &.selected {
-      box-shadow: 0 0 1em 0px $hover-color, 0 0 .25em 0px $hover-color inset;
-      background: #fff3;
-    }
-
-    &.selected {
-      cursor: default;
-    }
-  }
 }
 
 a {
--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -25,3 +25,34 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@use '../scss/theme';
+$hover-color: #556b2f44;
+
+#nav {
+  background: theme.$background-header;
+  color: theme.$color-text-dark;
+  padding: 1em;
+  text-align: center;
+  font-size: 1.25em;
+
+  a {
+    text-decoration: none;
+    padding: .5em 1.5em;
+    margin: .5em .5em;
+    text-transform: lowercase;
+    font-variant: small-caps;
+    transition: 0.2s;
+
+    &:hover, &.selected {
+      box-shadow: 0 0 1em 0px $hover-color, 0 0 .25em 0px $hover-color inset;
+      background: #fff3;
+    }
+
+    &.selected {
+      cursor: default;
+    }
+  }
+}
+</style>
