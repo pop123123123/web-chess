5f802dcb6c57e882110cdb036b086231f7318af7
Antoine Brûlé <antoine.brule@netc.eu>
2021-06-08 13:42:34 +0200

    add vue-fontawesome

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import notifications from '@kyvg/vue3-notification';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 createApp(App).use(store).use(router).use(notifications)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
